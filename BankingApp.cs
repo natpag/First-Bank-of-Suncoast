f31bf2e97a946cc49205a3d0ea8aae1e49b4cf9f
npagan <npagan@proforma.com>
2020-02-25 14:54:58 -0500

    Current broken version

---
+++ BankingApp.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+
+namespace First_Bank_of_Suncoast
+{
+  public class BankingApp
+  {
+
+  
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+
+namespace First_Bank_of_Suncoast
+{
+  class Program
+  {
+
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to the First Bank of Suncoast!");
+
+      var reader = new StreamReader("accounts.csv");
+      var data = reader.ReadToEnd();
+
+      Console.WriteLine("Here is your Account Summary:");
+      //should be able to see totals in my savings acct and checking acct
+      Console.WriteLine($"Savings Account:...   Checking Account:...");
+
+
+
+    }
+  }
+}
