6e56f6a9017d11081b1ee2b2feda29585f6c7cde
npagan <npagan@proforma.com>
2020-02-26 09:00:18 -0500

    Bank update

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CsvHelper;
+using System.Globalization;
 
 namespace First_Bank_of_Suncoast
 {
@@ -12,16 +13,38 @@ namespace First_Bank_of_Suncoast
 
     static void Main(string[] args)
     {
+      Console.Clear();
+
       Console.WriteLine("Welcome to the First Bank of Suncoast!");
 
       var reader = new StreamReader("accounts.csv");
-      var data = reader.ReadToEnd();
+      var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
 
-      Console.WriteLine("Here is your Account Summary:");
-      //should be able to see totals in my savings acct and checking acct
-      Console.WriteLine($"Savings Account:...   Checking Account:...");
+      bool appClosed = false;
+      while (!appClosed)
+      {
 
+        var input = Console.ReadLine().ToLower();
 
+        var writer = new StreamWriter("accounts.csv");
+        var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+        writer.Flush();
+
+        Console.WriteLine("Here is your Account Summary:");
+        //should be able to see totals in my savings acct and checking acct
+        Console.WriteLine($"Savings Account:...   Checking Account:...");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("\n(Deposit - D) (Withdraw - W) (Transfer - T)");
+
+        if (input == "d")
+        {
+          Console.WriteLine("Which Account would you like to deposit into? Checking (C) or Savings (S)");
+        }
+        else
+        {
+          appClosed = true;
+        }
+      }
 
     }
   }
---
+++ UserActions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using System.Globalization;
+
+namespace First_Bank_of_Suncoast
+{
+  class UserActions
+  {
+    public void DepositFunds()
+    {
+
+    }
+
+  }
+}
